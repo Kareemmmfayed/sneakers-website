b5d0928685aaf5f9c27fb81ef375a23da5e1582b
kareem <124769561+Kareemmmfayed@users.noreply.github.com>
2023-08-17 15:06:10 +0300

    Finished the navigation bar

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavBar from './components/NavBar';
+import Landing from './components/Landing'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <Landing />
+    </div>
+  );
+}
+
+export default App;
